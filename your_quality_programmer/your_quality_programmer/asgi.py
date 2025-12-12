f5c9b6677f1cea2b5c3c3659e4527cb3fe4fb13e
Brian Hornbrook <brian_hornbrook@outlook.com>
2025-12-12 16:56:15 -0600

    created your quality programmer django project

--- your_quality_programmer/your_quality_programmer/asgi.py
+++ your_quality_programmer/your_quality_programmer/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for HonestBusinesses project.
+ASGI config for your_quality_programmer project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HonestBusinesses.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_quality_programmer.settings')
 
 application = get_asgi_application()
