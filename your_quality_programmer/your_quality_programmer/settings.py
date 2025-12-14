6bccf174f44c8f594dac15290f076bedd3fa3804
Brian Hornbrook <brian_hornbrook@outlook.com>
2025-12-13 23:28:59 -0600

    configured postgres

--- your_quality_programmer/your_quality_programmer/settings.py
+++ your_quality_programmer/your_quality_programmer/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,8 +75,14 @@ WSGI_APPLICATION = 'your_quality_programmer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.{}'.format(
+            os.getenv('DATABASE_ENGINE', 'sqlite3')
+        ),
+        'NAME': os.getenv('DATABASE_NAME', 'name'),
+        'USER': os.getenv('DATABASE_USERNAME', 'username'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
+        'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DATABASE_PORT', 5432),
     }
 }
 
